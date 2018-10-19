cdd7a6bc549ede6fa370f61c3ed3f6d95a6696e4
adamSales <asales@utexas.edu>
2018-10-19 15:00:50 -0500

    median by subset

--- median.r
+++ median.r
@@ -3,13 +3,13 @@
 #### works better with large data (e.g. don't need "design" object)
 
 
-## finds median of x from ACS dataset dat
-## weights are dat$pwgtp
-## replication weights are dat$pwgtp[1-80]
-med <- function(x,dat){
-    x <-model.frame(x,dat)[[1]]
+## finds median of x from ACS sdataset sdat
+## weights are sdat$pwgtp
+## replication weights are sdat$pwgtp[1-80]
+med <- function(x,sdat,w1='pwgtp',wrep=paste0('pwgtp',1:80)){
+    x <-model.frame(x,sdat)[[1]]
     oo <- order(x)
-    w <- dat$pwgtp
+    w <- sdat[[w1]]
     cum.w <- cumsum(w[oo])/sum(w)
     Qf<-approxfun(cum.w,x[oo],method='linear',f=1,
                   yleft=min(x),yright=max(x),
@@ -18,10 +18,21 @@ med <- function(x,dat){
     point.est <-Qf(0.5)
     estfun<-as.numeric(x<point.est)
     est <- sum(w*estfun)/sum(w)
-    wr <- dat[,paste0('pwgtp',1:80)]
+    wr <- sdat[,wrep]
     reps <- apply(wr,2,function(ww) sum(estfun*ww)/sum(ww))
     se <- sqrt(4*mean((reps-est)^2))
     ci <- Qf(c(est+2*se,est-2*se))
     SE <- ((ci[1]-ci[2])/4)
-    c(point.est,SE)
+    c(point.est,SE,nrow(sdat))
+}
+
+medStr <- function(x,w1='pwgtp',wrep=paste0('pwgtp',1:80),subst,sdat,...){
+    x <- as.formula(paste('~',x))
+
+    if(!missing(subst)){
+        subst <- parse(text=subst)
+        subst <- eval(subst,sdat)
+        sdat <- sdat[subst, ]
+    }
+    med(x,sdat,w1=w1,wrep=wrep)
 }
