adbdb154e08f6936a0e1c5b17397110ce28ce127
adamSales <asales@utexas.edu>
2018-10-19 18:09:47 -0500

    end of day oct 19

--- estimationFunctions.r
+++ estimationFunctions.r
@@ -49,25 +49,34 @@ estSEstr <- function(x,w1='pwgtp',wrep=paste0('pwgtp',1:80),subst,sdat,na.rm=TRU
         svmean(x,w1,na.rm=na.rm)
     }
 
+    x <- eval(parse(text=x),sdat)
 
-    est <- mn(sdat[[x]],sdat[[w1]],subst,na.rm=na.rm)
+    est <- mn(x,sdat[[w1]],subst,na.rm=na.rm)
 
-    reps <- vapply(wrep, function(ww) mn(sdat[[x]],sdat[[ww]],subst,na.rm=na.rm),1.0)
+    reps <- vapply(wrep, function(ww) mn(x,sdat[[ww]],subst,na.rm=na.rm),1.0)
 
     se <- sqrt(mean((reps-est)^2)*4)
 
     n <- if(is.null(subst)){
-             if(na.rm) sum(!is.na(sdat[[x]])) else nrow(sdat)
-         } else if(na.rm) sum(!is.na(sdat[[x]][subst])) else sum(subst)
+             if(na.rm) sum(!is.na(x)) else nrow(sdat)
+         } else if(na.rm) sum(!is.na(x[subst])) else sum(subst)
     c(est,se,n)
 }
 
-svby <- function(x,fac,FUN,sdat,w1='pwgtp',wrep=paste0('pwgtp',1:80),...){
-    levs <- if(is.factor(sdat[[fac]])) levels(sdat[[fac]]) else sort(unique(sdat[[fac]]))
+svby <- function(x,fac,subsets,FUN,sdat,w1='pwgtp',wrep=paste0('pwgtp',1:80),prop=TRUE,...){
 
-    lapply(levs, function(ll){
-        subst <- paste(fac,'==',ll)
-        FUN(x=x,w1=w1,wrep=wrep,subst=subst,sdat=sdat,...)
-    })
+    if(!missing(fac)){
+        levs <- if(is.factor(sdat[[fac]])) levels(sdat[[fac]]) else sort(unique(sdat[[fac]]))
+        subsets <- paste(fac,levs)
+    }
+
+    out <- NULL
+    for(subst in subsets){
+        oo <- FUN(x=x,w1=w1,wrep=wrep,subst=subst,sdat=sdat,...)
+        if(prop) oo <- c( p =svmean(eval(parse(text=subst),sdat),sdat[[w1]])*100,oo) ## proportion point estimate
+        out <- rbind(out,oo)
+    }
+    rownames(out) <- if(exists("levs"))  levs else subsets
+    out
 }
 
